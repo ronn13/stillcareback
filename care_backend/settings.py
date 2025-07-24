86fe6a099aa82132b918da15f0cb4d9991103879
r0nn13 <delyon.ronnie@gmail.com>
2025-07-24 20:02:49 +0200

    cors origins updated

--- care_backend/settings.py
+++ care_backend/settings.py
@@ -181,7 +181,7 @@ REST_FRAMEWORK = {
 }
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://localhost:8081').split(',')
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://localhost:8081,https://web-production-83ebd.up.railway.app').split(',')
 
 CORS_ALLOW_CREDENTIALS = True
 
